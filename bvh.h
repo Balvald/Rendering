f991a2a9c276938a58fcb53a509221dc4b1c8de1
Florian Probst <derbalvald@gmail.com>
2025-06-18 12:33:46 +0200

    Update bvh.h

--- bvh.h
+++ bvh.h
@@ -100,16 +100,103 @@ class BoundingVolumeHierarchy
         return std::make_tuple(left_child, right_child);
     }
 
-    std::tuple<BoundingVolumeHierarchy, BoundingVolumeHierarchy> split_SAH(std::vector<Triangle> all_triangles, std::vector<Eigen::Vector3d> all_vertices)
+    std::tuple<BoundingVolumeHierarchy, BoundingVolumeHierarchy> split_SAH(std::vector<Triangle> all_triangles, std::vector<Eigen::Vector3d> all_vertices, BoundingVolumeHierarchy parent)
     {
         // SAH (Surface Area Heuristic) is a more complex algorithm that requires calculating the surface area of the bounding boxes
         // and determining the best split based on the distribution of triangles and vertices.
 
+        double best_cost = std::numeric_limits<double>::max();
+        double best_split_position = 0.0;
+        double best_axis = 0; // 0 for x, 1 for y, 2 for z
 
+        int num_buckets = 8; // Number of buckets for SAH
 
+        double C_trav = 1.0;
+        double C_intersect = 1.0;
 
+        double S_total_surface_area = surface_area(parent.bounding_box);
 
+        Eigen::Vector3d min = std::get<0>(parent.bounding_box);
+        Eigen::Vector3d max = std::get<1>(parent.bounding_box);
 
+        // Iterate over each axis (x, y, z)
+        for (int axis = 0; axis < 3; ++axis)
+        {
+            // Create buckets for the current axis
+            std::vector<std::vector<int>> buckets(num_buckets);
+            std::vector<double> bucket_surface_areas(num_buckets, 0.0);
+            std::vector<int> bucket_triangle_counts(num_buckets, 0);
+
+            // Distribute triangles into buckets based on their centroids
+            for (int index : parent.triangle_indices)
+            {
+                Triangle triangle = all_triangles[index];
+                Eigen::Vector3d centroid = (triangle.v1 + triangle.v2 + triangle.v3) / 3.0;
+
+                int bucket_index = static_cast<int>((centroid[axis] - min[axis]) / (max[axis] - min[axis]) * num_buckets);
+                bucket_index = std::clamp(bucket_index, 0, num_buckets - 1);
+
+                buckets[bucket_index].push_back(index);
+                bucket_triangle_counts[bucket_index]++;
+            }
+
+            // Calculate surface areas and costs for each split position
+            double left_area = 0.0;
+            double right_area = S_total_surface_area;
+
+            for (int i = 0; i < num_buckets - 1; ++i)
+            {
+                if (bucket_triangle_counts[i] > 0)
+                {
+                    Eigen::Vector3d left_min = min;
+                    Eigen::Vector3d left_max = max;
+                    left_max[axis] = min[axis] + (max[axis] - min[axis]) * (i + 1) / num_buckets;
+
+                    double left_bucket_area = surface_area(std::make_tuple(left_min, left_max));
+                    left_area += left_bucket_area * bucket_triangle_counts[i];
+                    right_area -= left_bucket_area * bucket_triangle_counts[i];
+
+                    double cost = C_trav + C_intersect * (left_area + right_area);
+                    if (cost < best_cost)
+                    {
+                        best_cost = cost;
+                        best_split_position = (min[axis] + max[axis]) / 2.0;
+                        best_axis = axis;
+                    }
+                }
+            }
+        }
+
+        // Create the left and right bounding boxes based on the best split found
+        Eigen::Vector3d left_min = std::get<0>(parent.bounding_box);
+        Eigen::Vector3d left_max = std::get<1>(parent.bounding_box);
+        Eigen::Vector3d right_min = std::get<0>(parent.bounding_box);
+        Eigen::Vector3d right_max = std::get<1>(parent.bounding_box);
+
+        if (best_axis == 0) // x-axis
+        {
+            left_max.x() = best_split_position;
+            right_min.x() = best_split_position;
+        }
+        else if (best_axis == 1) // y-axis
+        {
+            left_max.y() = best_split_position;
+            right_min.y() = best_split_position;
+        }
+        else if (best_axis == 2) // z-axis
+        {
+            left_max.z() = best_split_position;
+            right_min.z() = best_split_position;
+        }
+
+        BoundingVolumeHierarchy left_child(
+            std::make_tuple(left_min, left_max),
+            parent.triangle_indices, parent.vertex_indices, parent.parent_index, -1, -1);
+        BoundingVolumeHierarchy right_child(
+            std::make_tuple(right_min, right_max),
+            parent.triangle_indices, parent.vertex_indices, parent.parent_index, -1, -1);
+
+        return std::make_tuple(left_child, right_child);
     }
 
     double surface_area(std::tuple<Eigen::Vector3d, Eigen::Vector3d> box) const
