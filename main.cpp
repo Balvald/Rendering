e3acb635acf69cb580042e517499239a7ee5a304
Florian Probst <derbalvald@gmail.com>
2025-07-02 16:55:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -353,7 +353,7 @@ int main(int argc, char *argv[])
     }
 
     std::ofstream log_file;
-    std::string log_file_name = "log-" + std::to_string(method) + "-" + std::to_string(max_depth) + "-" + std::to_string(max_trig) + ".txt";
+    std::string log_file_name = "log-" + path.substr(9) + "-" + std::to_string(method) + "-" + std::to_string(max_depth) + "-" + std::to_string(max_trig) + ".txt";
     log_file.open(log_file_name);
     log_file << "Hello I am rank " << rank << " of " << size << "\n";
     log_file << "Camera aspect ratio: " << cam.get_aspect_ratio() << "\n";
