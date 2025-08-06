21f9572011629c3bfd205770f08544fec59ca920
Florian Probst <derbalvald@gmail.com>
2025-08-06 14:21:06 +0200

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -8,27 +8,39 @@ variable_args = [" --method", " --max-depth", " --max-trig", " --num-buckets"]
 
 method = [0, 1, 2, 3]
 method = [3]
-maximum_depth = 10
-maximum_trig = [1024, 512, 256, 128, 64, 32, 16, 8, 4]
+maximum_depth = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+maximum_trig = [1024]
 max_buckets = [1, 2, 4, 8, 10, 12, 14]
+max_buckets = [16, 18, 20, 22]
 
 program = ".\\raytracer.exe"
 
 
-for i in method:
-    for j in range(maximum_depth + 1):
-        for k in maximum_trig:
-            bucketstring = ""
-            if i == 3:
-                for l in max_buckets:
-                    bucketstring = variable_args[3] + " " + str(l) + " "
-            cmdline_args = base_cmdline_args + variable_args[0] + " " + str(i) + " " + \
-                                               variable_args[1] + " " + str(j) + " " + \
-                                               variable_args[2] + " " + str(k) + " " + \
-                                               bucketstring
+skipped_to_last = False
 
-            cmd = program + "".join(cmdline_args)
-            print("Running command:", cmd)
-            subprocess.run(cmd, shell=True)
-            print("Command finished.")
-            print("--------------------------------------------------")
+for i in method:
+    for depth in maximum_depth:
+        for j in range(depth + 1):
+            for k in maximum_trig:
+                for m in max_buckets:
+                    # if not skipped_to_last:
+                    # if i == 3 and j == 8 and k == 64 and m == 1:
+                    #     skipped_to_last = True
+                    #     print("Skipped to command for method", i, "depth", j, "trig", k, "buckets", m)
+                    # else:
+                    #     print("Skipping command for method", i, "depth", j, "trig", k, "buckets", m)
+                    #     continue
+                    bucketstring = ""
+                    if i == 3:
+                        bucketstring = variable_args[3] + " " + str(m) + " "
+                    cmdline_args = (base_cmdline_args +
+                                    variable_args[0] + " " + str(i) + " " +
+                                    variable_args[1] + " " + str(j) + " " +
+                                    variable_args[2] + " " + str(k) + " " +
+                                    bucketstring)
+
+                    cmd = program + "".join(cmdline_args)
+                    print("Running command:", cmd)
+                    subprocess.run(cmd, shell=True)
+                    print("Command finished.")
+                    print("--------------------------------------------------")
